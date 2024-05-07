36fe04fc49f12dd67464386104f8476dfe57e3b3
Alikhan Oitan <alikhan.oitan@gmail.com>
2024-05-07 16:00:48 +0500

    remove unused piece of code

--- src/auth/auth.guard.ts
+++ src/auth/auth.guard.ts
@@ -36,45 +36,6 @@ export class JwtGuard implements CanActivate {
     const [type, token] = request.headers['authorization'].split(' ') ?? [];
     return type === 'Bearer' ? token : undefined;
   }
-
-  // canActivate(
-  //   context: ExecutionContext,
-  // ): boolean | Promise<boolean> | Observable<boolean> {
-  //   const request = context.switchToHttp().getRequest();
-  //   const { authorization } = request.headers;
-
-  //   const [type, base64] = authorization.split(' ');
-
-  //   switch (type) {
-  //     case 'Basic':
-  //       return this.handleBasicAuth(base64);
-  //     case 'Bearer':
-  //       return this.handleBearerAuth(base64);
-  //     default:
-  //       return false;
-  //   }
-  // }
-
-  // private async handleBasicAuth(base64: string): Promise<boolean> {
-  //   console.log('Handling basic auth');
-  //   const [id] = Buffer.from(base64, 'base64').toString('ascii').split(':');
-  //   try {
-  //     await this.userService.findOneByOrFail(id);
-  //     // maybe can have an encrypted known message here
-  //     // decrypt with password and check if it matches
-  //     return true;
-  //   } catch (err) {
-  //     throw new NotFoundException('User not found', {
-  //       cause: err,
-  //       description: err.code || err.message,
-  //     });
-  //     return false;
-  //   }
-  // }
-
-  // private handleBearerAuth(base64: string) {
-  //   return false;
-  // }
 }
 
 @Injectable()
