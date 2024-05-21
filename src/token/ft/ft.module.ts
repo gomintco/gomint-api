6d1a5778ee5ed497b52df4b8c0e3143f6b6be60d
Jake Hall <jake.hallslm@gmail.com>
2024-05-21 21:00:46 +0100

    finished implementation of refactored create token transaction - need to test
    Signed-off-by: Jake Hall <jaycoolslm@gmail.com>

--- src/token/ft/ft.module.ts
+++ src/token/ft/ft.module.ts
@@ -6,9 +6,10 @@ import { UserModule } from 'src/user/user.module';
 import { KeyModule } from 'src/key/key.module';
 import { AuthModule } from 'src/auth/auth.module';
 import { AccountModule } from 'src/account/account.module';
+import { HederaModule } from 'src/hedera/hedera.module';
 
 @Module({
-  imports: [ClientModule, KeyModule, AuthModule, AccountModule],
+  imports: [ClientModule, KeyModule, AuthModule, AccountModule, HederaModule],
   providers: [FtService],
   controllers: [FtController],
 })
