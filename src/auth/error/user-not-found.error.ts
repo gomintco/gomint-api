782ad05aeda9b3fbccb433acce58d74f73144dcd
Alikhan Oitan <alikhan.oitan@gmail.com>
2024-06-17 17:26:25 +0500

    refactor get deal bytes

--- src/auth/error/user-not-found.error.ts
+++ src/auth/error/user-not-found.error.ts
@@ -1,10 +1,7 @@
 import { BaseError } from 'src/core/base.error';
 
 export class UserNotFoundError extends BaseError {
-  options: { cause: any; description: string };
-
-  constructor(message?: string, cause?: any, description?: string) {
+  constructor(message?: string) {
     super(message ?? 'User is not found');
-    this.options = { cause, description };
   }
 }
