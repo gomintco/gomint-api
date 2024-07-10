080b6bd029ac53dc67d1336492ec575807003432
Alikhan Oitan <alikhan.oitan@gmail.com>
2024-07-10 14:41:02 +0500

    fix dependencies issue

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -5,12 +5,31 @@ import { ApiKey } from './api-key.entity';
 import { UserModule } from 'src/user/user.module';
 import { AuthMediator } from './auth.mediator';
 import { ApiKeyService } from './api-key.service';
+import {
+  ApiKeyGuard,
+  EncryptionKeyGuard,
+  JwtGuard,
+  JwtOrApiKeyGuard,
+} from './auth.guard';
 
 @Global()
 @Module({
   imports: [TypeOrmModule.forFeature([ApiKey]), UserModule],
-  providers: [AuthMediator, ApiKeyService],
+  providers: [
+    AuthMediator,
+    ApiKeyService,
+    JwtGuard,
+    ApiKeyGuard,
+    JwtOrApiKeyGuard,
+    EncryptionKeyGuard,
+  ],
   controllers: [AuthController],
-  exports: [ApiKeyService],
+  exports: [
+    ApiKeyService,
+    JwtGuard,
+    ApiKeyGuard,
+    JwtOrApiKeyGuard,
+    EncryptionKeyGuard,
+  ],
 })
 export class AuthModule {}
