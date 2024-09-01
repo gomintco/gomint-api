35023d060f15957279e7089b5efe3b7c736efc3b
Jake Hall <jake.hallslm@gmail.com>
2024-09-01 06:51:32 +0100

    update web3 and use new proof parser
    Signed-off-by: Jake Hall <jaycoolslm@gmail.com>

--- src/ipfs/ipfs.service.ts
+++ src/ipfs/ipfs.service.ts
@@ -14,19 +14,17 @@ export class IpfsService {
     if (this.client) {
       return;
     }
-    const { parse } = await eval(`import('@ucanto/principal/ed25519')`);
+    const { Signer } = await eval(`import('@ucanto/principal/ed25519')`);
     const { create } = await eval(`import('@web3-storage/w3up-client')`);
-    const { parse: parseProof } = await eval(
-      `import('@web3-storage/w3up-client/proof')`,
-    );
+    const { parse } = await eval(`import('@web3-storage/w3up-client/proof')`);
     const { StoreMemory } = await eval(`import(
       '@web3-storage/w3up-client/stores/memory'
     )`);
 
-    const principal = parse(this.configService.ipfs.web3StorageKey);
+    const principal = Signer.parse(this.configService.ipfs.web3StorageKey);
     const store = new StoreMemory();
     const client = await create({ principal, store });
-    const proof = await parseProof(this.configService.ipfs.web3StorageProof);
+    const proof = await parse(this.configService.ipfs.web3StorageProof);
     const space = await client.addSpace(proof);
     await client.setCurrentSpace(space.did());
     this.client = client;
@@ -39,16 +37,5 @@ export class IpfsService {
     const blob = new Blob([JSON.stringify(metadata)]);
     const res = await this.client.uploadFile(blob);
     return 'ipfs://' + res.toString();
-  }
-
-  private async parseProof(data: string) {
-    const { importDAG } = await eval(`import('@ucanto/core/delegation')`);
-    const { CarReader } = await eval(`import('@ipld/car')`);
-    const blocks = [];
-    const reader = await CarReader.fromBytes(Buffer.from(data, 'base64'));
-    for await (const block of reader.blocks()) {
-      blocks.push(block);
-    }
-    return importDAG(blocks);
   }
 }
