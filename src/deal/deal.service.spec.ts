d0a593fa8764ffab7ae73a1fdf8374901cb80614
Alikhan Oitan <alikhan.oitan@gmail.com>
2024-10-31 17:57:08 +0500

    fix deal service tests
    Signed-off-by: Alikhan Oitan <alikhan.oitan@gmail.com>

--- src/deal/deal.service.spec.ts
+++ src/deal/deal.service.spec.ts
@@ -1,12 +1,37 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { DealService } from './deal.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Deal } from './deal.entity';
+import { AccountService } from '../account/account.service';
+import { KeyService } from '../key/key.service';
+import { AppConfigService } from '../config/app-config.service';
+import { Repository } from 'typeorm';
+import { createMock } from '@golevelup/ts-jest';
 
 describe('DealService', () => {
   let service: DealService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [DealService],
+      providers: [
+        DealService,
+        {
+          provide: getRepositoryToken(Deal),
+          useValue: createMock<Repository<Deal>>(),
+        },
+        {
+          provide: AccountService,
+          useValue: createMock<AccountService>(),
+        },
+        {
+          provide: KeyService,
+          useValue: createMock<KeyService>(),
+        },
+        {
+          provide: AppConfigService,
+          useValue: createMock<AppConfigService>(),
+        },
+      ],
     }).compile();
 
     service = module.get<DealService>(DealService);
