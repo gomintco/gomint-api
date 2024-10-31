b0d7df192a8808015b089d53a062d5a5be1423fb
Alikhan Oitan <alikhan.oitan@gmail.com>
2024-10-31 17:57:48 +0500

    fix ipfs service tests
    Signed-off-by: Alikhan Oitan <alikhan.oitan@gmail.com>

--- src/ipfs/ipfs.service.spec.ts
+++ src/ipfs/ipfs.service.spec.ts
@@ -1,12 +1,20 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { IpfsService } from './ipfs.service';
+import { AppConfigService } from '../config/app-config.service';
+import { createMock } from '@golevelup/ts-jest';
 
 describe('IpfsService', () => {
   let service: IpfsService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [IpfsService],
+      providers: [
+        IpfsService,
+        {
+          provide: AppConfigService,
+          useValue: createMock<AppConfigService>(),
+        },
+      ],
     }).compile();
 
     service = module.get<IpfsService>(IpfsService);
