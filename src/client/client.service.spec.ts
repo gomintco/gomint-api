7470aa547fdeeb1af812f63662dc86106b6cc5a2
Alikhan Oitan <alikhan.oitan@gmail.com>
2024-10-31 18:03:54 +0500

    fix client service tests
    Signed-off-by: Alikhan Oitan <alikhan.oitan@gmail.com>

--- src/client/client.service.spec.ts
+++ src/client/client.service.spec.ts
@@ -1,12 +1,19 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ClientService } from './client.service';
+import { AppConfigService } from 'src/config/app-config.service';
+import { createMock } from '@golevelup/ts-jest';
+import { KeyService } from 'src/key/key.service';
 
 describe('ClientService', () => {
   let service: ClientService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ClientService],
+      providers: [
+        ClientService,
+        { provide: AppConfigService, useValue: createMock<AppConfigService>() },
+        { provide: KeyService, useValue: createMock<KeyService>() },
+      ],
     }).compile();
 
     service = module.get<ClientService>(ClientService);
