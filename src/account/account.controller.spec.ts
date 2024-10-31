976faeb0019e060ad90a713f09c5727db8542da4
Alikhan Oitan <alikhan.oitan@gmail.com>
2024-10-31 17:56:08 +0500

    fix account controller tests
    Signed-off-by: Alikhan Oitan <alikhan.oitan@gmail.com>

--- src/account/account.controller.spec.ts
+++ src/account/account.controller.spec.ts
@@ -1,5 +1,10 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AccountController } from './account.controller';
+import { AccountService } from './account.service';
+import { AccountMediator } from './account.mediator';
+import { JwtOrApiKeyGuard, ApiKeyGuard } from '../auth/auth.guard';
+import { createMock } from '@golevelup/ts-jest';
+import { Logger } from '@nestjs/common';
 
 describe('AccountController', () => {
   let controller: AccountController;
@@ -7,7 +12,26 @@ describe('AccountController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [AccountController],
-    }).compile();
+      providers: [
+        {
+          provide: AccountService,
+          useValue: createMock<AccountService>(),
+        },
+        {
+          provide: AccountMediator,
+          useValue: createMock<AccountMediator>(),
+        },
+        {
+          provide: Logger,
+          useValue: createMock<Logger>(),
+        },
+      ],
+    })
+      .overrideGuard(JwtOrApiKeyGuard)
+      .useValue(createMock<JwtOrApiKeyGuard>())
+      .overrideGuard(ApiKeyGuard)
+      .useValue(createMock<ApiKeyGuard>())
+      .compile();
 
     controller = module.get<AccountController>(AccountController);
   });
